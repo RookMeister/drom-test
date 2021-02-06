f18534e1225b85034e48564021cee8a968685381
Artem Hmel <artemhmel6@gmail.com>
2021-02-06 19:15:35 +1000

    add get city date

--- src/actions/citiesActions.js
+++ src/actions/citiesActions.js
@@ -1,15 +1,36 @@
 export const GET_CITIES_REQUEST = 'GET_CITIES_REQUEST';
-export const GET_CITIES_SUCCESS = 'GET_CITIES_SUCCESS';
+export const GET_DATES_REQUEST = 'GET_DATES_REQUEST';
+export const SET_LOADING = 'SET_LOADING';
 
 export function getCities() {
   return async dispatch => {
-    dispatch({ type: GET_CITIES_REQUEST });
+    dispatch({ type: SET_LOADING });
     const url = 'https://www.mocky.io/v2/5b34c0d82f00007400376066?mocky-delay=700ms';
     let response = await fetch(url);
     let {cities} = await response.json();
     dispatch({
-      type: GET_CITIES_SUCCESS,
+      type: GET_CITIES_REQUEST,
       payload: cities,
+    });
+  }
+}
+
+export function getCityDate(id) {
+  return async dispatch => {
+    dispatch({ type: SET_LOADING });
+    const url = `https://www.mocky.io/v2/${id}?mocky-delay=700ms`;
+    let response = await fetch(url);
+    let {data} = await response.json();
+
+    const keyData = Object.keys(data);
+    const filtersData = keyData.map((item) => {
+      const hours = Object.values(data[item]);
+      return {date: item, hours: hours.filter((el) => !el.is_not_free)};
+    }).filter((item) => item.hours.length > 0);
+
+    dispatch({
+      type: GET_DATES_REQUEST,
+      payload: filtersData,
     });
   }
 }
