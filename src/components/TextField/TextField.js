aaffe03cb181586475fe0c8ab375a88c3da32649
Artem Hmel <artemhmel6@gmail.com>
2021-02-06 19:15:15 +1000

    edit text fields

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import './TextField.css';
 
-const TextField = ({name, placeholder, onChange, onBlur, error}) => (
-  <div className='text-field'>
+const TextField = ({name, placeholder, onChange, onBlur, error, type}) => {
+  const blur = () => onBlur(type);
+  return (
+    <div className='text-field'>
       <input
         type="text"
         name={name}
         placeholder={placeholder}
         onChange={onChange}
-        onBlur={onBlur}
+        onBlur={blur}
       />
-      {error && <div>{error}</div>}
-  </div>
-);
+      {error && <div className='error'>{error}</div>}
+    </div>
+  )
+};
 
 export default TextField;
