b1ac0cfd05663385ad637e620215488cb681bb01
Artem Hmel <artemhmel6@gmail.com>
2021-02-06 19:14:52 +1000

    edit city info

--- src/components/CityInfo/CityInfo.js
+++ src/components/CityInfo/CityInfo.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import Select from '../Select/Select';
+import  "./CityInfo.css";
 
-const CityInfo = ({value, placeholder, options, onClick, onBlur, onChange, error}) => {
-  console.log(123, placeholder);
+const CityInfo = ({value, options, onBlur, onChange, error, type}) => {
+  let placeholder = options && options[0] || { name: 'Владивосток' } ;
+  let address, phones, price = '';
+  if (value) {
+    address = value.address
+    phones = value.phones.map((phone) => <a key={phone} href={'tel:'+phone}>{phone}</a>)
+    phones = phones.length && phones.reduce((prev, curr) => [prev, ', ', curr]) || '';
+    price = value.price
+  }
   return (
     <div className='city-info'>
       <Select
+        type={type}
         value={value}
         placeholder={placeholder && placeholder.name }
         options={options}
         onChange={onChange}
-        onClick={onClick}
         onBlur={onBlur}
+        renderValue={(value) => value && value.name}
         error={error} />
-        <div>
-          {value && value.address}{<br/>}
-          {value && value.phones.join(', ')}<br/>
-          {value && value.price}<br/>
+        <div className='city-info__sub'>
+          {address && <p>{address} </p>}
+          {phones && <p>{phones}</p>}
+          {price && <p>Стоимость услуги {price} ₽</p>}
         </div>
     </div>
 )};
