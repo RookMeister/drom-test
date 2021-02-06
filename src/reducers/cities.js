de14ec2b44ff69dfa5de1d146762407ed8e0fa0f
Artem Hmel <artemhmel6@gmail.com>
2021-02-06 19:16:14 +1000

    add reducer

--- src/reducers/cities.js
+++ src/reducers/cities.js
@@ -1,17 +1,21 @@
-import { GET_CITIES_REQUEST, GET_CITIES_SUCCESS } from '../actions/citiesActions'
+import { GET_DATES_REQUEST, GET_CITIES_REQUEST, SET_LOADING } from '../actions/citiesActions'
 
 const initialState = {
-  cities: [],
+  cityOptions: [],
+  dateOptions: [],
   isFetching: false,
 }
 
 export function citiesReducer(state = initialState, action) {
   switch (action.type) {
-    case GET_CITIES_REQUEST:
+    case SET_LOADING:
       return { ...state, isFetching: true }
 
-    case GET_CITIES_SUCCESS:
-      return { ...state, cities: action.payload, isFetching: false }
+    case GET_CITIES_REQUEST:
+      return { ...state, cityOptions: action.payload, isFetching: false }
+
+    case GET_DATES_REQUEST:
+      return { ...state, dateOptions: action.payload, isFetching: false }
 
     default:
       return state
